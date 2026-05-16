4c3b3ae70a2a1630a345e71d18e9c11ff25b9516
amirmohamedboum@gmail.com <boumchitaabdoullah@gmail.com>
2026-05-16 10:54:16 +0100

    inscription

---
+++ index.js
@@ -0,0 +1,23 @@
+const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email");
+const inputPassword = document.getElementById("password");
+const inputAge = document.getElementById("age");
+const submitBtn = document.getElementById("submitbtn");
+
+console.log(inputName);
+console.log(inputEmail);
+console.log(inputPassword);
+console.log(inputAge);
+console.log(submitBtn);
+
+submitBtn.addEventListener("click", function (event) {  
+    event.preventDefault();
+    console.log("button clicked!");
+    console.log("Name: " + inputName.value);
+    console.log("Email: " + inputEmail.value);
+    console.log("Password: " + inputPassword.value);
+    console.log("Age: " + inputAge.value);
+
+
+    alert("Name: " + inputName.value + "\nEmail: " + inputEmail.value + "\nPassword: " + inputPassword.value + "\nAge: " + inputAge.value);
+});
